951a927d532c18c60dc741242833b82d74fb8541
XFNeo <xf.neo@mail.ru>
2019-09-09 14:51:17 +0400

    Added convertSize

--- src/ru/xfneo/FileUtility/Util/FileMetadataUtil.java
+++ src/ru/xfneo/FileUtility/Util/FileMetadataUtil.java
@@ -2,14 +2,16 @@ package ru.xfneo.FileUtility.Util;
 
 import ru.xfneo.FileUtility.Entity.FileMetadata;
 
+import java.math.RoundingMode;
 import java.nio.file.Path;
+import java.text.DecimalFormat;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class FileMetadataUtil {
 
-    public static List<FileMetadata> getDuplicateFiles(List<FileMetadata> allFiles){
+    public static List<FileMetadata> getDuplicateFiles(List<FileMetadata> allFiles) {
         return allFiles.stream()
                 .filter(x -> x.getPaths().size() > 1)
                 .collect(Collectors.toList());
@@ -33,9 +35,25 @@ public class FileMetadataUtil {
     public static void printFileMetadataList(List<FileMetadata> list) {
         for (FileMetadata fileMetadata : list) {
             System.out.println("File Name: " + fileMetadata.getFileName() +
-                    "\tSize: " + fileMetadata.getSize() +
+                    "\tSize: " + convertSize(fileMetadata.getSize()) +
                     "\tCount: " + fileMetadata.getCount() +
                     "\tPaths: " + fileMetadata.getPaths().stream().map(Path::toString).collect(Collectors.joining(", ")));
         }
+    }
+
+    public static String convertSize(long size) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.CEILING);
+
+        if (size >= 1024) { //KB
+            if (size >= 1024 * 1024) { //MB
+                if (size >= 1024 * 1024 *1024){ //GB
+                    return df.format((double)size/1024/1024/1024) + " GB";
+                }
+                return df.format((double)size/1024/1024) + " MB";
+            }
+            return df.format((double)size/1024) + " KB";
+        }
+        return size + " B";
     }
 }
